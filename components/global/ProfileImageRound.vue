a11351d2b780d7e5b599d365347a3930e78154cb
Wes Wilson <wes@iamweswilson.com>
2020-10-26 11:33:47 -0700

    Initial commit

---
+++ components/global/ProfileImageRound.vue
@@ -0,0 +1,14 @@
+<template>
+  <img :src="src" class="h-full w-full object-cover" />
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      default: () => 'https://via.placeholder.com/728x90.png?text=placeholder'
+    },
+  }
+}
+</script>
