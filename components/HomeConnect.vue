63889838401590efe4e7acb424d67f9506e9046e
Wes Wilson <wes@iamweswilson.com>
2020-10-28 14:06:58 -0700

    Adding LinkedIn

--- components/HomeConnect.vue
+++ components/HomeConnect.vue
@@ -1,11 +1,12 @@
 <template>
   <section>
     <h2>Connect with me</h2>
-    <p><a href="mailto:wes@iamweswilson.com">Email</a></p>
-    <p><a href="https://twitter.com/iamweswilson">Twitter</a></p>
-    <p><a href="https://www.instagram.com/iamweswilson">Instagram</a></p>
-    <p><a href="https://www.facebook.com/iamweswilson/">Facebook</a></p>
-    <p><a href="https://github.com/iamweswilson">Github</a></p>
+    <p><a href="mailto:wes@iamweswilson.com" target="_blank">Email</a></p>
+    <p><a href="https://twitter.com/iamweswilson" target="_blank">Twitter</a></p>
+    <p><a href="https://www.instagram.com/iamweswilson" target="_blank">Instagram</a></p>
+    <p><a href="https://www.facebook.com/iamweswilson/" target="_blank">Facebook</a></p>
+    <p><a href="https://www.linkedin.com/in/iamweswilson/" target="_blank">LinkedIn</a></p>
+    <p><a href="https://github.com/iamweswilson" target="_blank">Github</a></p>
   </section>
 </template>
 
