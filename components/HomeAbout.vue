0e3673ff94dc8e10f4640bf5dced34fa34118400
Wes Wilson <wes@iamweswilson.com>
2020-10-28 19:42:57 -0700

    Update About

--- components/HomeAbout.vue
+++ components/HomeAbout.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="my-10 md:my-24 text-center">
     <p>Hi! I am Wes Wilson. I am a designer, marketer and (at times) a front-end developer. Most of my energy goes to being the Director of Marketing for the newly minted <a href="https://openinfra.dev" target="_blank">Open Infrastructure Foundation</a> where we build  communities who write open source software that runs in production.</p>
-    <p>Previously, I founded a <a href="https://brandcrowd.com" target="_blank">start-up</a> (sold in 2011), was Director of Product at <a href="https://venturelab.org/" target="_blank">VentureLab</a>, helped build ZippyKid (now <a href="https://pressable.com" target="_blank">Pressable</a>), and spent a few years trying to make a living as a freelance designer.</p>
+    <p>Previously, I founded a <a href="https://brandcrowd.com" target="_blank">start-up</a> (sold in 2011), was Director of Product at <a href="https://venturelab.org/" target="_blank">VentureLab</a>, helped build ZippyKid (now <a href="https://pressable.com" target="_blank">Pressable</a>), was a mentor for [Techstars](http://techstars.com) Cloud, and spent a few years trying to make a living as a freelance designer.</p>
   </section>
 </template>
 
