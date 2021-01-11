034ff3449070e5e56b67d2fb78fac42f73432750
Wes Wilson <wes@iamweswilson.com>
2021-01-11 15:15:22 -0800

    updates to 'focus' project

--- pages/projects/features.vue
+++ pages/projects/features.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="mt-12 mb-1 rounded-xl bg-white p-8">
+  <div class="feature-wrapper mt-12 mb-1 rounded-xl bg-white py-16 px-8 shadow-2xl items-center">
     <ul>
       <li>
-        <div class="title">
+        <div class="title fast">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
           class="icon lightning">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
@@ -12,7 +12,7 @@
         <p>You might feel like this gives “moving at the speed of light” a bad name. Just sayin’.</p>
       </li>
       <li>
-        <div class="title">
+        <div class="title tested">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="icon fire">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
@@ -22,7 +22,7 @@
         <p>We’ve run it through the coals so many times, we almost feel bad... but we don’t.</p>
       </li>
       <li>
-        <div class="title">
+        <div class="title secure">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="icon fingerprint">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4" />
           </svg>
@@ -41,19 +41,30 @@
 </script>
 
 <style lang="scss" scoped>
+  .feature-wrapper {
+    max-height: 350px;
+    overflow: hidden;
+    transition: all 500ms ease;
+    @media (max-width: 640px) {
+      height: auto;
+    }
+  }
   .least {
     ul {
       li {
         display: flex;
         flex-direction: column;
         margin-bottom: 1rem;
+        transition: all 500ms ease;
         &:last-child {
           margin-bottom: 0;
         }
         .title {
-          flex: 1 1 0px;
+          transition: all 500ms ease;
+          border-right: 1px solid transparent;
           .icon {
-            display: none;
+            width: 0;
+            transition: all 500ms ease;
           }
         }
         h2 {
@@ -62,10 +73,12 @@
           font-size: 1em;
           text-transform: none;
           margin-bottom: 0.1rem;
+          padding-right: 0;
+          transition: all 500ms ease;
         }
         p {
+          transition: all 500ms ease;
           color: #9A9A9A;
-          flex: 1 1 37%;
           font-size: 0.9em;
           margin: 0.1rem 0;
         }
@@ -79,6 +92,7 @@
         flex-direction: row;
         padding-bottom: 0;
         margin-bottom: 0;
+        transition: all 500ms ease;
         &:first-child {
           .title {
             padding-top: 0,
@@ -103,17 +117,45 @@
           flex-direction: row;
           align-items: center;
           padding: 1rem 0;
+          transition: 
+            all 500ms ease,
+            border-color 0s ease-out 0.5s;
           @media (max-width: 640px) {
             flex-direction: column;
             text-align: center;
             justify-content: center;
             min-width: 100px;
           }
+          &.fast {
+            h2 {
+              color: #6E44E7;
+            }
+            .icon {
+              stroke: #6E44E7;
+            }
+          }
+          &.tested {
+            h2 {
+              color: #E78844;
+            }
+            .icon {
+              stroke: #E78844;
+            }
+          }
+          &.secure {
+            h2 {
+              color: #DE0404;
+            }
+            .icon {
+              stroke: #DE0404;
+            }
+          }
           .icon {
             stroke: #162c40;
             display: inline;
             width: 2rem;
             margin-right: .4rem;
+            transition: all 500ms ease;
             @media (max-width: 640px) {
               width: 35px;
               margin-right: 0;
@@ -128,14 +170,15 @@
             font-size: 1em;
             text-transform: none;
             margin-bottom: 0;
+            transition: all 500ms ease;
           }
         }
         p {
+          transition: all 500ms ease;
           color: #333;
-          flex: 1 1 37%;
           font-size: 0.9em;
           margin: 1rem 0 0 2rem;
-          align-items: center;
+          transition: all 500ms ease;
         }
       }
     }
@@ -145,6 +188,7 @@
       display: flex;
       flex-direction: row;
       text-align: center;
+      transition: all 500ms ease;
       @media (max-width: 640px) {
         flex-direction: column;
       }
@@ -152,6 +196,7 @@
         display: flex;
         flex-direction: column;
         padding: 0 1rem;
+        transition: all 500ms ease;
         @media (max-width: 640px) {
           margin-bottom: 2rem;
           &:last-child {
@@ -163,10 +208,13 @@
           flex-direction: column;
           align-items: center;
           justify-content: center;
+          transition: all 500ms ease;
+          border: 0px solid transparent;
           .icon {
             display: block;
             width: 5rem;
             margin-bottom: 1rem;
+            transition: all 500ms ease;
             &.lightning {
               stroke: #6E44E7;
             }
@@ -180,6 +228,7 @@
           h2 {
             margin-bottom: 1rem;
             color: #162c40;
+            transition: all 500ms ease;
           }
         }
         p {
@@ -187,6 +236,7 @@
           color: #444;
           font-size: 0.9em;
           align-items: center;
+          transition: all 500ms ease;
         }
       }
     }
---
+++ pages/projects/ui-by-focus.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container mx-auto">
+    <div class="flex justify-center">
+      <div class="w-full my-8 lg:w-7/12">
+        <TheHeader />
+        <div class="project-inner my-8">
+          <h2>Project: Changing UI based on user's focus</h2>
+          <p>I've found the most difficult part of creating marketing pages is gaining and keeping a reader's focus. Focus can be gained through writing, design, and placement. This exercise digs into that challenge a bit, looking at how visual hierarchy, color, spacing, and type can be adjusted based on how much focus you want to bring to that component is to your page. </p>
+          <p>
+            The idea is that you'd be able to quickly adjust any components to match the level of importance you perceive it having on the page. This is a fairly basic example, but let's assume you have a small "features" component on your marketing page. The content is the same across each variant, but the attention brought to the component changes based on how much focus you believe the item should get.
+          </p>
+          <features :class="setClass" />
+          <section class="text-center">
+            <h3 class="font-medium mt-12">How much focus do you want to give this component on your page?</h3>
+            <div class="block">
+              <button @click="changeClass('least')" :class="{active: isActive == 'least'}">Minimum</button>
+              <button @click="changeClass('mid')" :class="{active: isActive == 'mid'}">Medium</button>
+              <button @click="changeClass('most')" :class="{active: isActive == 'most'}">Maximum</button>
+            </div>
+          </section>
+          <div>
+            <p>As you can see, the desired focus you set would dictate colors, icons, and text placement. On a live page, the level of importance would likley also change this component's placement on the page. </p>
+            <p>Related, but not shown in this exercise, is that giving more focus to one component would in turn reduce the focus to another epsecially if the components are within the same viewport.</p>
+          </div>
+        </div>
+        <hr>
+        <TheFooter />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import features from './features.vue'
+  export default {
+    components: { features },
+    props: [
+      'buttonValue',
+    ],
+    data: function() {
+      return {
+        setClass: 'least',
+        isActive: 'least',
+      }
+    },
+    methods: {
+      changeClass: function(value) {
+        this.setClass = value
+        this.isActive = value
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  button {
+    font-size: 0.9em;
+    font-weight: bold;
+    margin: 1rem 2rem;
+    background: var(--code);
+    color: var(--code-color);
+    padding: 0.5rem;
+    border-radius: 999px;
+    &.active {
+      background: var(--color-primary);
+      color: var(--bg);
+    }
+    &:first-child {
+      margin-left: 0
+    }
+    &:last-child {
+      margin-right: 0;
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+  section {
+    margin: 1rem 0 4rem;
+  }
+</style>
