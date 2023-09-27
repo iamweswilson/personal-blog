45294c9ce4f2fb9ccefc56254715a1868713f93b
Wes Wilson <wes@iamweswilson.com>
2023-09-27 11:33:17 -0700

    contact and footer updates

--- components/HomeConnect.vue
+++ components/HomeConnect.vue
@@ -2,11 +2,10 @@
   <section>
     <h2>Connect with me</h2>
     <p><a href="mailto:wes@iamweswilson.com" target="_blank">Email</a></p>
-    <p><a href="https://twitter.com/iamweswilson" target="_blank">Twitter</a></p>
+    <p><a href="https://twitter.com/iamweswilson" target="_blank">Twitter/X</a></p>
     <p><a href="https://www.instagram.com/iamweswilson" target="_blank">Instagram</a></p>
     <p><a href="https://www.facebook.com/iamweswilson/" target="_blank">Facebook</a></p>
     <p><a href="https://www.linkedin.com/in/iamweswilson/" target="_blank">LinkedIn</a></p>
-    <p><a href="https://dribbble.com/iamweswilson" target="_blank">Dribbble</a></p>
     <p><a href="https://github.com/iamweswilson" target="_blank">Github</a></p>
   </section>
 </template>
