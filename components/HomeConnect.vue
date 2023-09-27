b3b153f22cba634f91870f99995ffb09aa0a026f
Wes Wilson <wes@iamweswilson.com>
2023-09-27 13:04:14 -0700

    profile pic

--- components/HomeConnect.vue
+++ components/HomeConnect.vue
@@ -1,12 +1,17 @@
 <template>
-  <section>
-    <h2>Connect with me</h2>
-    <p><a href="mailto:wes@iamweswilson.com" target="_blank">Email</a></p>
-    <p><a href="https://twitter.com/iamweswilson" target="_blank">Twitter/X</a></p>
-    <p><a href="https://www.instagram.com/iamweswilson" target="_blank">Instagram</a></p>
-    <p><a href="https://www.facebook.com/iamweswilson/" target="_blank">Facebook</a></p>
-    <p><a href="https://www.linkedin.com/in/iamweswilson/" target="_blank">LinkedIn</a></p>
-    <p><a href="https://github.com/iamweswilson" target="_blank">Github</a></p>
+  <section class="grid grid-cols-2 gap-4">
+    <main>
+      <h2>Connect with me</h2>
+      <p><a href="mailto:wes@iamweswilson.com" target="_blank">Email</a></p>
+      <p><a href="https://twitter.com/iamweswilson" target="_blank">Twitter/X</a></p>
+      <p><a href="https://www.instagram.com/iamweswilson" target="_blank">Instagram</a></p>
+      <p><a href="https://www.facebook.com/iamweswilson/" target="_blank">Facebook</a></p>
+      <p><a href="https://www.linkedin.com/in/iamweswilson/" target="_blank">LinkedIn</a></p>
+      <p><a href="https://github.com/iamweswilson" target="_blank">Github</a></p>
+    </main>
+    <aside class="flex justify-end">
+      <img src="/img/profile-pic.jpg" class="block h-40 w-40 origin-right rounded-full" alt="">
+    </aside>
   </section>
 </template>
 
