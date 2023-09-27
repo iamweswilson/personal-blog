c18aa3294f88acfc39f65ffc0ccd21ec33c2f1cc
Wes Wilson <wes@iamweswilson.com>
2023-09-27 11:29:00 -0700

    Removing articles from homepage

--- pages/index.vue
+++ pages/index.vue
@@ -2,39 +2,6 @@
   <div>
         <HomeAbout />
         <hr>
-        <section>
-          <h2>Thoughts, designs & other stuff</h2>
-          <p class="text-sm">NOTE: This is where I write about random technology, things I've created or anything silly that comes to mind. <em>This is not a reflection of what I do at my "real" job, but might be a reflection of what I do on my "free" time.</em>
-          </p>
-          <ul class="flex flex-wrap mt-6">
-            <li
-              v-for="article of articles"
-              :key="article.slug"
-              class="mt-3 xs:w-full md:w-1/2 px-2 xs:mb-6 md:mb-12 article-card"
-            >
-              <NuxtLink
-                :to="{ name: 'blog-slug', params: { slug: article.slug } }"
-                class="flex h-full transition-shadow duration-150 ease-in-out shadow-sm hover:shadow-md xxlmax:flex-col overflow-hidden"
-              >
-                <img
-                  v-if="article.img"
-                  class="md:h-64 h-auto xxlmin:w-1/2 xxlmax:w-full object-cover post-img"
-                  :src="article.img"
-                />
-
-                <div
-                  class="p-6 flex flex-col justify-between xxlmin:w-1/2 xxlmax:w-full"
-                >
-                  <h3 class="font-bold">{{ article.title }}</h3>
-                  <p class="font-bold text-gray-600 text-sm">
-                    {{ article.description }}
-                  </p>
-                </div>
-              </NuxtLink>
-            </li>
-          </ul>
-      </section>
-        <hr>
         <HomeConnect />
   </div>
 </template>
