1793966f745266776e4a7eb1c18edf5888d23c6e
Wes Wilson <wes@iamweswilson.com>
2023-09-27 13:52:48 -0700

    typo and connect sort

--- components/HomeConnect.vue
+++ components/HomeConnect.vue
@@ -3,11 +3,11 @@
     <main>
       <h2>Connect with me</h2>
       <p><a href="mailto:wes@iamweswilson.com" target="_blank">Email</a></p>
+      <p><a href="https://www.linkedin.com/in/iamweswilson/" target="_blank">LinkedIn</a></p>
+      <p><a href="https://github.com/iamweswilson" target="_blank">Github</a></p>
       <p><a href="https://twitter.com/iamweswilson" target="_blank">Twitter/X</a></p>
       <p><a href="https://www.instagram.com/iamweswilson" target="_blank">Instagram</a></p>
       <p><a href="https://www.facebook.com/iamweswilson/" target="_blank">Facebook</a></p>
-      <p><a href="https://www.linkedin.com/in/iamweswilson/" target="_blank">LinkedIn</a></p>
-      <p><a href="https://github.com/iamweswilson" target="_blank">Github</a></p>
     </main>
     <aside class="flex justify-end">
       <img src="/img/profile-pic.jpg?nf_resize=fit&w=130&h=130" class="block h-40 w-40 origin-right rounded-full" alt="">
