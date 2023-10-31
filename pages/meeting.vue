90a286d6be7aa450d3d255aad51004ed6c5893d6
Wes Wilson <wes@iamweswilson.com>
2023-10-31 13:06:22 -0700

    test meeting room

---
+++ pages/meeting.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <iframe src="https://iamweswilson.whereby.com/test3d159202f-164d-45d6-bbbb-224821375fde" ?background=off allow="camera; microphone; fullscreen; speaker; display-capture; compute-pressure" style="height: 700px; width: 100%"></iframe>
+  </div>
+</template>
