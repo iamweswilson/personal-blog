7348fa59140629370fe9940da3f746d0f636a4a6
Wes Wilson <wes@iamweswilson.com>
2025-09-26 18:41:05 -0700

    Updating with CoS text

--- components/HomeAbout.vue
+++ components/HomeAbout.vue
@@ -3,13 +3,7 @@
     <p>I am Wes Wilson.
     </p>
     <p>
-      I am a versatile and strategic leader who excels at handling multiple responsibilities and roles. My track record demonstrates my ability to drive organizational success across various domains and contexts. I am committed to delivering excellence and adapting to changing needs and challenges in any endeavor.
-    </p>
-    <p>
-      As the VP of Operations for the <a href="https://openinfra.dev" target="_blank">OpenInfra Foundation</a> (formerly OpenStack Foundation), I am responsible for overseeing daily operations and ensuring that our product strategy delivers value to our member organizations. I am the steward of the OpenInfra brand and work closely with our team to support communities that create open source solutions for focus areas such as cloud computing, edge, container security, and CI/CD. Our most notable project is OpenStack, which powers some of the world’s most critical infrastructure, including CERN, Walmart, Nike, Bloomberg, Blizzard, and more.
-    </p>
-    <p>
-      In my role as a leader in the open-source community, I have spearheaded several initiatives that have enhanced collaboration and innovation around open infrastructure. These include developing the OpenInfra Project Funds—a model for hosting and supporting open-source projects—and launching regional hubs OpenInfra Europe in Belgium and OpenInfra Asia in Singapore. These hubs serve as platforms for connecting and empowering local stakeholders.
+      Operations leader serving as Chief of Staff at the <a href="//:openinfra.org">OpenInfra Foundation</a> (part of the <a href="//:linuxfoundation.org.org">Linux Foundation</a>). I lead cross-functional initiatives spanning finance, legal, and vendor management, while managing and evolving the core operations that drive business success. With entrepreneurial roots and hands-on experience across multiple departments, I bring a full business perspective that helps me align teams, leadership, and the board to solve problems and deliver results at global scale. I focus on building efficient systems and operational frameworks that create measurable value for our corporate members, developers, and users worldwide.
     </p>
     <p>
       Previously, I founded a <a href="https://brandcrowd.com" target="_blank">start-up</a> (acquired 2011), was Director of Product at <a href="https://venturelab.org/" target="_blank">VentureLab</a>, helped build ZippyKid (now <a href="https://pressable.com" target="_blank">Pressable</a>), was a mentor for <a href="https://techstars.com/" target="_blank">Techstars</a> Cloud, and spent a few years as a freelance consultant/designer.</p>
